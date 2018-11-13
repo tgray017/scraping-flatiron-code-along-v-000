185ba50475ad33c175895a40fe1bb800e852f07f
tgray017 <tgray017@gmail.com>
2018-11-13 03:10:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
     end
     
     def make_courses
-      courses = get_page.css('article')
+      courses = get_courses.css('article')
       get_courses.each do |courses_array|
         courses_array.each do |course_object| 
           binding.pry
